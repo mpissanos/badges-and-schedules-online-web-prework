c26201837558b57aaf776289a8b4f59724333110
Manny Pissanos <mpissano@gmail.com>
2018-09-05 22:54:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def assign_rooms(names)
   names.map.with_index {|name , index| "Hello, #{name}! You'll be assigned to room #{index + 1}!" }
 end
 
-# def printer(names)
-#   print batch_badge_creator(names)
-#   assign_rooms.each { |i| print "#{i}" }
-# end
+def printer(names)
+  print batch_badge_creator(names)
+  assign_rooms.each { |i| print "#{i}" }
+end
