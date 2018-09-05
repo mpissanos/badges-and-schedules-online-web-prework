8e9eceb34f4e9ad3ec017728d4002337e13b9939
Manny Pissanos <mpissano@gmail.com>
2018-09-05 18:26:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(names)
-  names.map.with_index {|name , index| "Hello,#{name}! You'll be assigned to room #{index}!" }
+  names.map.with_index {|name , index| "Hello,#{name}! You'll be assigned to room #{index + 1}!" }
 end
 
   
